1a5b51902fed85c3868ef809eea0c1e13ea5edfc
Thkeya Smith <thkeyasmith@gmail.com>
2024-05-12 23:17:26 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        Open up App.js to start working on your app!
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
+
+export default App;
